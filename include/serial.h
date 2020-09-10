d7337f26319ac4d2d6078e058c0014eacea5429b
Nicola Serlonghi <nicola@serlonghi.com>
2020-09-10 18:27:44 +0200

    [FIX] Bug fix

--- include/serial.h
+++ include/serial.h
@@ -1,28 +1,8 @@
 #ifndef _SERIAL_H
 #define _SERIAL_H
 
-void serial(int m, int n, int nnz, int *csrRowPtr, int *csrColIdx, double *csrVal, int *cscColPtr, int *cscRowIdx, double *cscVal) {
-    int *curr = new int[n]();
-    for(int i = 0; i < m; i++) {
-        for(int j = csrRowPtr[i]; j < csrRowPtr[i + 1]; j++) {
-            cscColPtr[csrColIdx[j] + 1]++;
-        }
-    }
-    
-    for(int i = 1; i < n + 1; i++) {
-        cscColPtr[i] += cscColPtr[i - 1];
-    }
+#include <iostream>
 
-    for(int i = 0; i < m; i++) {
-        for(int j = csrRowPtr[i]; j < csrRowPtr[i + 1]; j++) {
-            int loc = cscColPtr[csrColIdx[j]] + curr[csrColIdx[j]]++;
-            cscRowIdx[loc] = i;
-            cscVal[loc] = csrVal[j];
-        }
-    }
-
-    free(curr);
-}
-
+void serial(int m, int n, int nnz, int *csrRowPtr, int *csrColIdx, double *csrVal, int *cscColPtr, int *cscRowIdx, double *cscVal);
 
 #endif
